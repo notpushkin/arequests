795dcd8900a384040ae61c7b68bebf787f21c3f4
Alexander Pushkov <hi@ale.rocks>
2017-05-02 19:53:24 +0300

    Unlicense

--- setup.py
+++ setup.py
@@ -1,16 +1,20 @@
 from distutils.core import setup
 
+__version__ = "0.1.1"
+
 setup(
-    name='arequests',
-    version='0.1.0',
-    author='Ale',
-    author_email='ale@songbee.net',
-    url='https://github.com/iamale/arequests',
+    name="arequests",
+    version=__version__,
+    author="Alexander Pushkov",
+    author_email="hi@ale.rocks",
+    url="https://github.com/iamale/arequests",
     description="Agnostic API classes for Requests.",
-    py_modules=['arequests'],
-    install_requires=[
-        'requests',
+    py_modules=["arequests"],
+    install_requires=["requests"],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: Public Domain",
+        "Programming Language :: Python",
     ],
-    license='Apache 2.0',
-    keywords=['github', 'api']
 )
